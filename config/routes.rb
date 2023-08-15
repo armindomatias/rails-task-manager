b25f9a85099637fc34d5a4ac6dd3bfd2c70d7099
Armindo Matias <armindomatiasg@gmail.com>
2023-08-15 14:51:55 +0100

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :task_index
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'tasks/new', to: 'tasks#new', as: :task_new
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
